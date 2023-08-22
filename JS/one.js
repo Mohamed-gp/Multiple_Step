d2c86bad069d2e43497589f07776dd703b2fc605
Mohamed-gp <mohamedterba6@gmail.com>
2023-08-22 23:50:34 +0100

    not completed yet

--- JS/one.js
+++ JS/one.js
@@ -6,13 +6,31 @@ submit.onclick = function (e) {
   for (let i = 0; i < input.length; i++) {
     if (input[i].value == "") {
       required[i].classList.remove("hidden");
-      input[i].classList.add(".outline-add")
+      input[i].classList.add("outline-add")
+      console.log(input[i].classList)
       bool = false;
     } else {
       required[i].classList.add("hidden");
+      input[i].classList.remove("outline-add")
     }
   }
   if (bool == false) {
     e.preventDefault();
   }
 };
+
+
+
+
+
+//step two 
+let plans = document.querySelectorAll(".plans > div")
+
+plans.forEach(function(e){
+  e.addEventListener("click",function(){
+    if (e.classList.contains("active-plan")) {
+      
+    }
+    e.classList.add("active-plan")
+  })
+})
--- README.md
+++
@@ -1,1 +0,0 @@
-# Multiple_Step
