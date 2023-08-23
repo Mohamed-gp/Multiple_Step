4ea598012f3b7f5fb58db444872d61ef379c0ab6
Mohamed-gp <mohamedterba6@gmail.com>
2023-08-23 23:57:45 +0100

    not completed yet

--- JS/one.js
+++ JS/one.js
@@ -1,11 +1,50 @@
-let input = document.querySelectorAll("input");
-let submit = document.querySelector(".submit");
-let required = document.querySelectorAll(".required");
-submit.onclick = function (e) {
-  let bool = true;
-  for (let i = 0; i < input.length; i++) {
-    if (input[i].value == "") {
-      required[i].classList.remove("hidden");
+//step two 
+if (document.title == "Frontend Mentor | step 2" ) {
+  let plans = document.querySelectorAll(".plans > div")
+  
+  plans.forEach(function(e){
+    e.addEventListener("click",function(){
+      plans.forEach(function(r){
+        r.classList.remove("active-plan")
+      })
+      e.classList.add("active-plan")
+    })
+  })
+  
+  let centerChild = document.querySelector(".child-circle")
+  let bonus = document.querySelectorAll(".bonus")
+  let monthly = document.querySelector(".monthly")
+  let yearly = document.querySelector(".yearly")
+  yearly.onclick = function(){
+    centerChild.classList.remove("left-1")
+    centerChild.classList.add("right-1")
+    monthly.classList.remove("left-1","font-bold","text-Marineblue")
+    yearly.classList.add("left-1","font-bold","text-Marineblue")
+    bonus.forEach(function(e){
+      e.classList.remove("hidden")
+    })
+  }
+  monthly.onclick = function(){
+    centerChild.classList.remove("right-1")
+    centerChild.classList.add("left-1","font-bold","text-Marineblue")
+    yearly.classList.remove("left-1","font-bold","text-Marineblue")
+    monthly.classList.add("left-1","font-bold","text-Marineblue")
+
+    bonus.forEach(function(e){
+      e.classList.add("hidden")
+    })
+  }
+}
+
+
+  let input = document.querySelectorAll("input");
+  let submit = document.querySelector(".submit");
+  let required = document.querySelectorAll(".required");
+  submit.onclick = function (e) {
+    let bool = true;
+    for (let i = 0; i < input.length; i++) {
+      if (input[i].value == "") {
+        required[i].classList.remove("hidden");
       input[i].classList.add("outline-add")
       console.log(input[i].classList)
       bool = false;
@@ -23,14 +62,3 @@ submit.onclick = function (e) {
 
 
 
-//step two 
-let plans = document.querySelectorAll(".plans > div")
-
-plans.forEach(function(e){
-  e.addEventListener("click",function(){
-    if (e.classList.contains("active-plan")) {
-      
-    }
-    e.classList.add("active-plan")
-  })
-})
